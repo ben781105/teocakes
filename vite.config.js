7ca6cbe9757c62974bac4b090095799cac9c5e7d
benard <benardleno78@gmail.com>
2026-09-12 13:46:26 +0300

    vite.config.js fix

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import { viteImageOptimizer } from "vite-plugin-image-optimizer";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    viteImageOptimizer({
+    ViteImageOptimizer({
       jpg: { quality: 75 },
       jpeg: { quality: 75 },
       png: { quality: 75 },
